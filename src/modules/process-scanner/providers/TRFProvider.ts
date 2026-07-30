70d9c5eaae2bbc70cc2bc85554f3dcf296b40dc8
W1 CAPITAL <daviconcentrix@gmail.com>
2026-07-30 13:44:17 -0300

    Delete src/modules/process-scanner directory

--- src/modules/process-scanner/providers/TRFProvider.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileOverview Provider Especializado para Tribunais Regionais Federais
- */
-
-import { CNJProvider } from './CNJProvider';
-
-export class TRFProvider extends CNJProvider {
-  // Implementação específica para TRFs se necessário no futuro
-}
