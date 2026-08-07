da63d8eded393cc13e379b527e78c5ec16bb1273
W1 CAPITAL <daviconcentrix@gmail.com>
2026-08-07 13:08:14 -0300

    Delete components directory

--- components/providers/motion-root.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import { MotionProvider } from "@/components/ui/motion";
-export function MotionRoot({ children }: { children: React.ReactNode }) {
-  return <MotionProvider>{children}</MotionProvider>;
-}
