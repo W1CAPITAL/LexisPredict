9b4d85b77ce08d34c9c0dc94b53a04dbd6d73632
W1 CAPITAL <daviconcentrix@gmail.com>
2026-08-07 13:07:41 -0300

    Delete lib directory

--- lib/score-advogado.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @copyright 2026 Davi Alves Figueredo / W1 Capital Assessoria Financeira Ltda.
- * @license Proprietary - All rights reserved.
- * ARQUIVO REDIRECIONADO PARA score-engine.ts
- */
-export * from "./score-engine";
